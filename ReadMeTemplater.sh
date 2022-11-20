b05ee65360b68178ad726d8de2283e10e325cb69
jaredreyes039 <jaredreyes039@gmail.com>
2022-11-19 22:13:45 -0700

    Temp. removing stack functionality for later improvements

--- ReadMeTemplater.sh
+++ ReadMeTemplater.sh
@@ -5,22 +5,6 @@ echo "Let's get you started, how about starting with a description?"
 echo "Please, enter a brief, but detailed description of your code to share with others: "
 read desc
 echo ""
-echo "Perfect! Now, let's make it clear what the code consists of."
-echo "Are you ready to enter your stack? (Y/n)"
-read permission
-idx=0
-STACK=()
-while [ $permission == "Y" ]; do
-    echo "Enter stack tool: "
-    read tool
-
-    STACK[$idx]="$tool"
-    ((idx+=1))
-
-    echo "Add another tool?"
-    read permission
-
-done
 echo "Awesome. Next, are there any warnings or disclaimers we need to know? (y/n)"
 read warning_status
 echo ""
@@ -46,10 +30,6 @@ Created By: $authors
 - $disclaimer
 
     " >> README.md
-    echo "# Stack" >> README.md
-    for ((i=0; i< $stack_count; i++)); do
-        echo "- ${STACK[$i]}" >> README.md
-        done
 elif [  $warning_status == "n"  ]; then
     echo "Aight, just a bit longer..."
     echo "Hmm..."
@@ -69,12 +49,6 @@ Created By: $authors
 {Insert **USAGE INSTRUCTIONS** here}
 
     " >> README.md
-
-    echo "## Stack" >> README.md
-    for ((i=0; i< $stack_count; i++)); do
-        echo "- ${STACK[$i]}" >> README.md
-        done
-    
 else
     echo "Error: Not a valid option. No will be used as default."
     echo "Aight, just a bit longer..."
@@ -93,9 +67,5 @@ else
 
 
     " >> README.md
-    echo "## Stack" >> README.md
-    for ((i = 0; i < $stack_count; i++)); do
-        echo "- ${STACK[$i]} " >> README.md
-        done
 fi
 echo "Now, just go into your README file, check the syntax, add any last details, and fill out the USAGE section!"
